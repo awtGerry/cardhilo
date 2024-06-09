9b345e58c89bf56a3883ed277c460bd2e602bed7
Victor Rodriguez <awtgerry@gmail.com>
2024-06-09 15:43:27 -0600

    nix files

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? (import <nixpkgs> {}).pkgs }:
+with pkgs;
+mkShell {
+  buildInputs = [
+    python3Packages.virtualenv # run virtualenv .
+    python3Packages.pyqt5 # avoid installing via pip
+    python3Packages.pyusb # fixes the pyusb 'No backend available' when installed directly via pip
+  ];
+  shellHook = ''
+    # fixes libstdc++ issues and libgl.so issues
+    LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib/:/run/opengl-driver/lib/
+    # fixes xcb issues :
+    QT_PLUGIN_PATH=${qt5.qtbase}/${qt5.qtbase.qtPluginPrefix}
+  '';
+}
