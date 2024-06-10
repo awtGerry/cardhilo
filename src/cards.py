c9a869c0f8a4705632b6153620a07226889e2e9b
Victor Rodriguez <awtGerry@gmail.com>
2024-06-10 10:35:56 -0600

    add source

---
+++ src/cards.py
@@ -0,0 +1,269 @@
+import cv2
+import numpy as np
+
+# Adaptive threshold levels
+BKG_THRESH = 60
+CARD_THRESH = 30
+
+# Width and height of card corner, where rank and suit are
+CORNER_WIDTH = 32
+CORNER_HEIGHT = 84
+
+# Dimensions of rank train images
+RANK_WIDTH = 70
+RANK_HEIGHT = 125
+
+# Dimensions of suit train images
+SUIT_WIDTH = 70
+SUIT_HEIGHT = 100
+
+RANK_DIFF_MAX = 2000
+SUIT_DIFF_MAX = 700
+
+CARD_MAX_AREA = 120000
+CARD_MIN_AREA = 25000
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# Structures to hold query card and train card information
+class Query_card:
+    def __init__(self):
+        self.contour = []
+        self.width, self.height = 0, 0
+        self.corner_pts = []
+        self.center = []
+        self.warp = []
+        self.rank_img = []
+        self.suit_img = []
+        self.best_rank_match = "Unknown"
+        self.best_suit_match = "Unknown"
+        self.rank_diff = 0
+        self.suit_diff = 0
+
+class Train_ranks:
+    def __init__(self):
+        self.img = None
+        self.name = "Placeholder"
+
+class Train_suits:
+    def __init__(self):
+        self.img = None
+        self.name = "Placeholder"
+
+def load_ranks(filepath):
+    train_ranks = []
+    i = 0
+    for Rank in ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
+              "Eight", "Nine", "Ten", "Jack", "Queen", "King"]:
+        train_ranks.append(Train_ranks())
+        train_ranks[i].name = Rank
+        filename = filepath + Rank + '.jpg'
+        train_ranks[i].img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+        i += 1
+
+    print("Ranks loaded")
+    return train_ranks
+
+def load_suits(filepath):
+    train_suits = []
+    i = 0
+    for Suit in ['Hearts', 'Diamonds', 'Clubs', 'Spades']:
+        train_suits.append(Train_suits())
+        train_suits[i].name = Suit
+        filename = filepath + Suit + '.jpg'
+        train_suits[i].img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+        i += 1
+
+    print("Suits loaded")
+    return train_suits
+
+def preprocess_image(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    img_w, img_h = np.shape(image)[:2]
+    bkg_level = blur[int(img_h / 100)][int(img_w / 2)]
+    thresh_level = bkg_level + BKG_THRESH
+    retval, thresh = cv2.threshold(blur, thresh_level, 255, cv2.THRESH_BINARY)
+
+    return thresh
+
+def find_cards(thresh_image):
+    cnts, hier = cv2.findContours(thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    index_sort = sorted(range(len(cnts)), key=lambda i: cv2.contourArea(cnts[i]), reverse=True)
+    if len(cnts) == 0:
+        return [], []
+
+    cnts_sort = []
+    hier_sort = []
+    cnt_is_card = np.zeros(len(cnts), dtype=int)
+
+    for i in index_sort:
+        cnts_sort.append(cnts[i])
+        hier_sort.append(hier[0][i])
+
+    for i in range(len(cnts_sort)):
+        size = cv2.contourArea(cnts_sort[i])
+        peri = cv2.arcLength(cnts_sort[i], True)
+        approx = cv2.approxPolyDP(cnts_sort[i], 0.01 * peri, True)
+
+        if ((size < CARD_MAX_AREA) and (size > CARD_MIN_AREA)
+            and (hier_sort[i][3] == -1) and (len(approx) == 4)):
+            cnt_is_card[i] = 1
+
+    return cnts_sort, cnt_is_card
+
+def preprocess_card(contour, image):
+    qCard = Query_card()
+    qCard.contour = contour
+    peri = cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, 0.01 * peri, True)
+
+    pts = np.float32(approx)
+    s = pts.sum(axis=2)
+    tl = pts[np.argmin(s)]
+    br = pts[np.argmax(s)]
+    diff = np.diff(pts, axis=-1)
+    tr = pts[np.argmin(diff)]
+    bl = pts[np.argmax(diff)]
+
+    if tl[0][0] > 10:
+        qCard.corner_pts = [tl, tr, br, bl]
+
+    qCard.width, qCard.height = find_card_dimensions(qCard.corner_pts)
+
+    qCard.center = find_card_center(qCard.corner_pts)
+
+    qCard.warp = flattener(image, qCard.corner_pts, qCard.width, qCard.height)
+
+    Qcorner = qCard.warp[0:CORNER_HEIGHT, 0:CORNER_WIDTH]
+    Qcorner_zoom = cv2.resize(Qcorner, (0, 0), fx=2.5, fy=2.5)
+
+    gray_corner = cv2.cvtColor(Qcorner_zoom, cv2.COLOR_BGR2GRAY)
+    blur_corner = cv2.GaussianBlur(gray_corner, (5, 5), 0)
+    retval, thresh_corner = cv2.threshold(blur_corner, CARD_THRESH, 255, cv2.THRESH_BINARY)
+
+    Qrank_roi = thresh_corner[20:185, 0:64]
+    Qsuit_roi = thresh_corner[186:336, 0:64]
+    Qrank_cnts, hier = cv2.findContours(Qrank_roi, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    Qrank_cnts = sorted(Qrank_cnts, key=cv2.contourArea, reverse=True)
+
+    if len(Qrank_cnts) != 0:
+        x, y, w, h = cv2.boundingRect(Qrank_cnts[0])
+        Qrank = Qrank_roi[y:y + h, x:x + w]
+        qCard.rank_img = cv2.resize(Qrank, (RANK_WIDTH, RANK_HEIGHT), 0, 0)
+
+    Qsuit_cnts, hier = cv2.findContours(Qsuit_roi, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    Qsuit_cnts = sorted(Qsuit_cnts, key=cv2.contourArea, reverse=True)
+    if len(Qsuit_cnts) != 0:
+        x, y, w, h = cv2.boundingRect(Qsuit_cnts[0])
+        Qsuit = Qsuit_roi[y:y + h, x:x + w]
+        qCard.suit_img = cv2.resize(Qsuit, (SUIT_WIDTH, SUIT_HEIGHT), 0, 0)
+
+    return qCard
+
+def match_card(qCard, train_ranks, train_suits):
+    best_rank_match_diff = 10000
+    best_suit_match_diff = 10000
+    best_rank_match_name = "Unknown"
+    best_suit_match_name = "Unknown"
+
+    if len(qCard.rank_img) != 0:
+        for Trank in train_ranks:
+            diff_img = cv2.absdiff(qCard.rank_img, Trank.img)
+            rank_diff = int(np.sum(diff_img) / 255)
+            if rank_diff < best_rank_match_diff:
+                best_rank_match_diff = rank_diff
+                best_rank_match_name = Trank.name
+
+    if len(qCard.suit_img) != 0:
+        for Tsuit in train_suits:
+            diff_img = cv2.absdiff(qCard.suit_img, Tsuit.img)
+            suit_diff = int(np.sum(diff_img) / 255)
+            if suit_diff < best_suit_match_diff:
+                best_suit_match_diff = suit_diff
+                best_suit_match_name = Tsuit.name
+
+    qCard.best_rank_match = best_rank_match_name
+    qCard.best_suit_match = best_suit_match_name
+    qCard.rank_diff = best_rank_match_diff
+    qCard.suit_diff = best_suit_match_diff
+
+    return qCard
+
+def draw_results(image, qCard):
+    x = qCard.center[0]
+    y = qCard.center[1]
+    # cv2.putText(image, qCard.best_rank_match, (x - 60, y + 20), font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+    # cv2.putText(image, qCard.best_suit_match, (x - 60, y + 40), font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText(image, (qCard.best_rank_match+" of"), (x-60, y-10), font, 0.7, (0,255,0), 2, cv2.LINE_AA)
+    cv2.putText(image, qCard.best_suit_match, (x-60, y+20), font, 0.7, (0,255,0), 2, cv2.LINE_AA)
+    return image
+
+# def match_card(qCard, train_ranks, train_suits):
+#     best_rank_match_diff = 10000
+#     best_suit_match_diff = 10000
+#     best_rank_match_name = "Unknown"
+#     best_suit_match_name = "Unknown"
+#     for Trank in train_ranks:
+#         diff_img = cv2.absdiff(qCard.rank_img, Trank.img)
+#         rank_diff = int(np.sum(diff_img) / 255)
+#         if rank_diff < best_rank_match_diff:
+#             best_rank_match_diff = rank_diff
+#             best_rank_match_name = Trank.name
+#
+#     for Tsuit in train_suits:
+#         diff_img = cv2.absdiff(qCard.suit_img, Tsuit.img)
+#         suit_diff = int(np.sum(diff_img) / 255)
+#         if suit_diff < best_suit_match_diff:
+#             best_suit_match_diff = suit_diff
+#             best_suit_match_name = Tsuit.name
+#
+#     qCard.best_rank_match = best_rank_match_name
+#     qCard.best_suit_match = best_suit_match_name
+#     qCard.rank_diff = best_rank_match_diff
+#     qCard.suit_diff = best_suit_match_diff
+#
+#     return qCard
+
+# def print_results(qCard, image):
+#     rank_name = qCard.best_rank_match
+#     suit_name = qCard.best_suit_match
+#     rank_diff = qCard.rank_diff
+#     suit_diff = qCard.suit_diff
+#
+#     if rank_diff < RANK_DIFF_MAX:
+#         rank_name = rank_name + " " + str(rank_diff)
+#         cv2.putText(image, rank_name, (qCard.center[0] - 60, qCard.center[1] + 20), font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+#
+#     if suit_diff < SUIT_DIFF_MAX:
+#         suit_name = suit_name + " " + str(suit_diff)
+#         cv2.putText(image, suit_name, (qCard.center[0] - 60, qCard.center[1] + 40), font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+#
+#     return image
+
+def find_card_dimensions(corner_pts):
+    # width = np.linalg.norm(corner_pts[1] - corner_pts[0])
+    # height = np.linalg.norm(corner_pts[2] - corner_pts[1])
+    width = np.sqrt(((corner_pts[0][0][0] - corner_pts[1][0][0])**2) + ((corner_pts[0][0][1] - corner_pts[1][0][1])**2))
+    height = np.sqrt(((corner_pts[0][0][0] - corner_pts[3][0][0])**2) + ((corner_pts[0][0][1] - corner_pts[3][0][1])**2))
+    return int(width), int(height)
+
+def find_card_center(corner_pts):
+    center_x = int((corner_pts[0][0][0] + corner_pts[2][0][0]) / 2)
+    center_y = int((corner_pts[0][0][1] + corner_pts[2][0][1]) / 2)
+    return center_x, center_y
+
+def flattener(image, corner_pts, width, height):
+    tmp_rect = np.zeros((4, 2), dtype="float32")
+    s = np.sum(corner_pts, axis=2)
+    diff = np.diff(corner_pts, axis= -1)
+    tmp_rect[0] = corner_pts[np.argmin(s)]
+    tmp_rect[2] = corner_pts[np.argmax(s)]
+    tmp_rect[1] = corner_pts[np.argmin(diff)]
+    tmp_rect[3] = corner_pts[np.argmax(diff)]
+    maxWidth = width
+    maxHeight = height
+    dst = np.array([[0, 0], [maxWidth - 1, 0], [maxWidth - 1, maxHeight - 1], [0, maxHeight - 1]], dtype="float32")
+    M = cv2.getPerspectiveTransform(tmp_rect, dst)
+    warp = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    return warp
--- src/main.py
+++ src/main.py
@@ -1,21 +1,86 @@
 import cv2
+import numpy as np
+import requests
+import os
+from io import BytesIO
+from PIL import Image
 
-# URL of the video stream from ESP32-CAM
-url = "http://192.168.100.124:81/stream"
+import cards
 
-# Open the video stream
-cap = cv2.VideoCapture(url)
+# ESP32-CAM URL
+CAM_URL = "http://192.168.100.124/cam-hi.jpg"  # Change to your ESP32-CAM IP
 
+# Function to capture image from ESP32-CAM
+def capture_image(url):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    img = np.array(img)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return img
+
+# Load the ranks and suits
+path = os.path.dirname(os.path.abspath(__file__))
+train_ranks = cards.load_ranks("src/imgs/")
+train_suits = cards.load_suits("src/imgs/")
+
+freq = cv2.getTickFrequency()
+
+# Main loop
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Failed to grab frame")
-        break
+    # Load the image from the ESP32-CAM
+    image = capture_image(CAM_URL)
 
-    cv2.imshow('ESP32-CAM', frame)
+    # timer to calculate framerate
+    t1 = cv2.getTickCount()
 
+    # Preprocess the image
+    pre_proc = cards.preprocess_image(image)
+
+    # Find and sort the contours of the cards
+    cnts_sort, cnt_is_card = cards.find_cards(pre_proc)
+
+    # If there are contours
+    if len(cnts_sort) != 0:
+        # Initialize the list of cards
+        cards_list = []
+        k = 0
+
+        # for each contour detected
+        for i in range(len(cnts_sort)):
+            if (cnt_is_card[i] == 1):
+                # preprocess card
+                cards_list.append(cards.preprocess_card(cnts_sort[i], image))
+                print("Card detected")
+
+                # find best rank and suit matches
+                # cards_list[k].best_rank_match, cards_list[k].best_suit_match, cards_list[k].rank_diff, cards_list[k].suit_diff = cards.match_card(cards_list[k], train_ranks, train_suits)
+
+                # draw center point and match result on the card
+                image = cards.draw_results(image, cards_list[k])
+                k += 1
+                print("Card: ", k)
+
+        # Draw card contours on the image
+        if (len(cards_list) != 0):
+            temp_cnts = []
+            for i in range(len(cards_list)):
+                temp_cnts.append(cards_list[i].contour)
+            cv2.drawContours(image, temp_cnts, -1, (255, 0, 0), 2)
+
+    # Calculate framerate
+    cv2.putText(image, "FPS: {0:.2f}".format(cv2.getTickFrequency() / (cv2.getTickCount() - t1)), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+
+    # Display the image
+    cv2.imshow("Card Detector", image)
+
+    t2 = cv2.getTickCount()
+    time1 = (t2 - t1) / freq
+    frame_rate_calc = 1 / time1
+
+    # Press 'q' to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.release()
+# Release the camera and close the window
 cv2.destroyAllWindows()
+print("Done")
